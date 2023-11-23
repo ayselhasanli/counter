b305f1d78a934faf30785c560e4a9dd60a3ac4b1
ayselhasanli <aysiiihasan@gmail.com>
2023-11-23 10:38:46 +0400

    mini js project - counter

---
+++ index.js
@@ -0,0 +1,27 @@
+let count = 0;
+let value = document.getElementById("count");
+
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
+
+    value.textContent = count;
+
+    if (count < 0) {
+      value.style.color = "red";
+    } else if (count > 0) {
+      value.style.color = "green";
+    } else {
+      value.style.color = "black";
+    }
+  });
+});
